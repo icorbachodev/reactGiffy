cd7752653122151ac81c265f894be89bc04356fc
ikkotheunforgiven <ikkotheunforgiven@gmail.com>
2022-09-17 13:20:55 +0200

    Primeros pasos React

---
+++ src/components/ListOfGifs.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState }  from "react";
+import Gif from "./Gif";
+import getGifs from "../services/getGifs";
+
+export default function ListOfGifs({ keyword }) {
+    const [gifs, setGifs] = useState([])
+
+    useEffect(() => {
+      getGifs({ keyword }).then(gifs => setGifs(gifs))
+    }, [keyword])
+
+    return (
+        gifs.map(singleGif => <Gif key={singleGif.id} id={singleGif.id} title={singleGif.title} url={singleGif.url} />)
+    )
+}
