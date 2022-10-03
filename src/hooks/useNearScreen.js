c53507081ce6e6eb430f02f1832f43487a00afb6
ikkotheunforgiven <ikkotheunforgiven@gmail.com>
2022-10-03 16:29:09 +0200

    Infinity Scroll hecho

--- src/hooks/useNearScreen.js
+++ src/hooks/useNearScreen.js
@@ -1,16 +1,20 @@
 import { useEffect, useState, useRef } from "react"
 
 
-export default function useNearScreen({ distance = '100px' } = {}) {
+export default function useNearScreen({ distance = '100px', externalRef, once = true } = {}) {
     const [isNearScreen, setShow] = useState(false)
     const fromRef = useRef()
 
     useEffect(() => {
+        const element = externalRef ? externalRef.current : fromRef.current
+
         const onChange = (entries, observer) => {
             const el = entries[0]
             if(el.isIntersecting) {
                 setShow(true)
-                observer.disconnect()
+                once && observer.disconnect()
+            } else {
+                !once && setShow(false)
             }
         }
 
@@ -18,7 +22,7 @@ export default function useNearScreen({ distance = '100px' } = {}) {
             rootMargin: distance
         })
 
-        observer.observe(fromRef.current)
+        observer.observe(element)
 
         return () => observer.disconnect()
     })
--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import ListOfGifs from "components/ListOfGifs";
 import { useGifs } from "hooks/useGifs";
+import useNearScreen from "hooks/useNearScreen";
+import debounce from "just-debounce-it";
 
 export default function SearchResults({ params }) {
     const { keyword } = params
     const { gifs, setPage } = useGifs({ keyword })
+    const externalRef = useRef()
+    const {isNearScreen} = useNearScreen({ externalRef, once: false })
 
-    const handleNextPage = () => setPage(prevPage => prevPage + 1)
+    const debounceHandleNextPage = useCallback(debounce(() => setPage(prevPage => prevPage + 1), 200), [])
+
+    useEffect(() => {
+        if(isNearScreen) debounceHandleNextPage()
+    }, [debounceHandleNextPage, isNearScreen])
 
     return <>
         {
             <>
                 <h3 className="App-title">Resultados de "{decodeURI(keyword)}"</h3>
                 <ListOfGifs gifs={gifs} />
+                <div id="visor" ref={externalRef}></div>
             </>
         }
-        <button onClick={handleNextPage}>Get next page</button>
     </>
 }
